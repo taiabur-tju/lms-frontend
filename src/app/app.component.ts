8e5f68bdacf559b7ecd42a0d9c123b4d7a942a90
Taiabur Rahman <taiabur-tju@qq.com>
2018-12-01 11:26:44 +0800

    frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+
+  serverData: JSON;
+  employeeData: JSON;
+  employee:JSON;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+    })
+  }
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON;
+      console.log(this.employeeData);
+    })
+  }
+  getEmployee(empid) {
+    this.httpClient.get('http://127.0.0.1:5002/employees/'+empid).subscribe(data => {
+      this.employee = data as JSON;
+      console.log(this.employee);
+    })
+  }
+}
